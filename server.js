6f6f06bf4bf7d129a723bb7c53bb3949a1345c5d
Levi Giraud <giraudlevi@gmail.com>
2019-12-08 20:40:02 -0700

    added logic to enable editing of employee data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,13 +14,6 @@ connection.connect(function (err) {
     if (err) throw err;
 
 });
-
-// function queryAllRoles() {
-//     connection.query("SELECT * FROM role", function(err, res) {
-//       if (err) throw err;
-//       console.log(res);
-//     });
-//   }
 
 function chooseAction() {
     inquirer
@@ -42,6 +36,190 @@ function chooseAction() {
         });
 }
 
+function updateEmployee() {
+    connection.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "employeeName",
+                    message: "Please select the employee you want to edit.",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].id + " " + results[i].first_name + " " + results[i].last_name);
+                        }
+                        return choiceArray;
+                    }
+                }
+            ])
+            .then(function (data) {
+                for (var i = 0; i < results.length; i++) {
+                    if (data.employeeName === results[i].id + " " + results[i].first_name + " " + results[i].last_name) {
+                        var employeeID = results[i].id;
+                    }
+                }
+                return (employeeID);
+            })
+            .then(function (employeeID) {
+                inquirer
+                    .prompt([
+                        {
+                            type: "list",
+                            name: "chooseEdit",
+                            message: "What would you like to edit for this employee?",
+                            choices: ['First name', 'Last name', 'Role']
+                        }
+                    ])
+                    .then(function (data) {
+                        if (data.chooseEdit === "First name") {
+                            newFirstName(employeeID);
+                        } else if (data.chooseEdit === "Last name") {
+                            newLastName(employeeID);
+                        } else {
+                            newEmpRole(employeeID);
+                        }
+                    });
+            });
+    })
+}
+
+function newEmpRole(employeeID) {
+    connection.query("SELECT * FROM role", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    type: "rawlist",
+                    name: "roleID",
+                    message: "What is their new role.",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].title);
+                        }
+                        return choiceArray;
+                    }
+                }
+            ])
+            .then(function (data) {
+                for (var i = 0; i < results.length; i++) {
+                    if (data.roleID === results[i].title) {
+                        data.roleID = results[i].id
+                    }
+                }
+                return data;
+            })
+            .then(function (data) {
+                connection.query(
+                    "UPDATE employee SET ? WHERE ?",
+                [
+                  {
+                    role_id: data.roleID
+                  },
+                  {
+                    id: employeeID
+                  }
+                ],
+                function(error) {
+                  if (error) throw err;
+                  chooseAction();
+                }
+              );
+            });
+    })
+}
+
+function newLastName(employeeID) {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "newLast",
+                message: "What is their new last name?"
+            }
+        ])
+        .then(function (data) {
+            connection.query(
+                "UPDATE employee SET ? WHERE ?",
+                [
+                  {
+                    last_name: data.newLast
+                  },
+                  {
+                    id: employeeID
+                  }
+                ],
+                function(error) {
+                  if (error) throw err;
+                  chooseAction();
+                }
+              );
+        });
+}
+
+function newFirstName(employeeID) {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "newFirst",
+                message: "What is their new first name?"
+            }
+        ])
+        .then(function (data) {
+            connection.query(
+                "UPDATE employee SET ? WHERE ?",
+                [
+                  {
+                    first_name: data.newFirst
+                  },
+                  {
+                    id: employeeID
+                  }
+                ],
+                function(error) {
+                  if (error) throw err;
+                  chooseAction();
+                }
+              );
+        });
+}
+
+function viewData() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "chooseTable",
+                message: "What would you like to see?",
+                choices: ['Departments', 'Roles', 'Employees']
+            }
+        ])
+        .then(function (data) {
+            if (data.chooseTable === "Departments") {
+                connection.query("SELECT * FROM department", function (err, results) {
+                    if (err) throw err;
+                    console.table(results);
+                    chooseAction();
+                });
+            } else if (data.chooseTable === "Roles") {
+                connection.query("SELECT * FROM role", function (err, results) {
+                    if (err) throw err;
+                    console.table(results);
+                    chooseAction();
+                });
+            } else {
+                connection.query("SELECT * FROM employee", function (err, results) {
+                    if (err) throw err;
+                    console.table(results);
+                    chooseAction();
+                });
+            }
+        });
+}
+
 function newInput() {
     inquirer
         .prompt([
@@ -55,7 +233,7 @@ function newInput() {
         .then(function (data) {
             if (data.chooseInput === "Department") {
                 newDepartment();
-            } else if (data.chooseAction === "Role") {
+            } else if (data.chooseInput === "Role") {
                 newRole();
             } else {
                 newEmployee();
@@ -87,32 +265,56 @@ function newDepartment() {
 }
 
 function newRole() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "title",
-                message: "Please enter the new roles's title.",
-            },
-            {
-                type: "number",
-                name: "salary",
-                message: "Please enter the new roles's salary.",
-            }
-        ])
-        .then(function (data) {
-            connection.query(
-                "INSERT INTO role SET ?",
+    connection.query("SELECT * FROM department", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
                 {
-                    title: data.title,
-                    salary: data.salary
+                    type: "input",
+                    name: "title",
+                    message: "Please enter the new roles's title.",
                 },
-                function (err) {
-                    if (err) throw err;
-                    chooseAction();
+                {
+                    type: "number",
+                    name: "salary",
+                    message: "Please enter the new roles's salary.",
+                },
+                {
+                    type: "rawlist",
+                    name: "departmentID",
+                    message: "Please enter the new role's department.",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].name);
+                        }
+                        return choiceArray;
+                    }
                 }
-            );
-        });
+            ])
+            .then(function (data) {
+                for (var i = 0; i < results.length; i++) {
+                    if (data.departmentID === results[i].name) {
+                        data.departmentID = results[i].id
+                    }
+                }
+                return data;
+            })
+            .then(function (data) {
+                connection.query(
+                    "INSERT INTO role SET ?",
+                    {
+                        title: data.title,
+                        salary: data.salary,
+                        department_id: data.departmentID
+                    },
+                    function (err) {
+                        if (err) throw err;
+                        chooseAction();
+                    }
+                );
+            });
+    })
 }
 
 function newEmployee() {
